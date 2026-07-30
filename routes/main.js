feac8a798c464bc52beac64e4de1f4b430a2fc33
ItsCiaPro <josuemedrado193@gmail.com>
2026-07-29 23:55:29 -0300

    add reset.css & main page

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-   res.render('user');
+   res.render('main');
 });
 
 module.exports = router;
